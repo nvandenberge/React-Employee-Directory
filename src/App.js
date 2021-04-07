1eb8e34cfcd3de1d2f1944c48c27cfa2cc935d6a
Nick Vandenberge <nvandenberge14@gmail.com>
2021-04-07 14:54:37 -0400

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
+import React from 'react';
 import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+      <Header />
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="wrapper">
+      <header className="navbar justify-content-center">
+        <h1>Employee Directory</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
