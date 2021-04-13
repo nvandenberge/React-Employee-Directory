72cc540afbaeff4a2dac4e803d648a2a0b06254d
Nick Vandenberge <nvandenberge14@gmail.com>
2021-04-13 14:29:16 -0400

    fixed sort warning

--- src/components/Sort/Sort.js
+++ src/components/Sort/Sort.js
@@ -1,18 +1,14 @@
 import React from "react";
 
-const Sort = ({ handleSort, header}) => {
-return (
-    <div>
-        <th> {header}
-        <button
-            onClick={handleSort}
-            className="bg-gray rounded ml-1"
-        >
-            Sort
-        </button>
-        </th>
-    </div>
-)
-}
+const Sort = ({ handleSort, header }) => {
+  return (
+    <th>
+      {header}
+      <button onClick={handleSort} className="bg-gray rounded ml-1">
+        Sort
+      </button>
+    </th>
+  );
+};
 
 export default Sort;
